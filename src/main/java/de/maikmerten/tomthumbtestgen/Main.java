ae824a22692fb0a46710f55a097b5bc4008d7824
Maik Merten <maikmerten@googlemail.com>
2016-10-22 11:50:12 +0200

    allow specifying the number of tests within a test collection via the first command line argument

--- src/main/java/de/maikmerten/tomthumbtestgen/Main.java
+++ src/main/java/de/maikmerten/tomthumbtestgen/Main.java
@@ -10,7 +10,15 @@ public class Main {
 	
 	public static void main(String[] args) throws Exception {
 		
-		TestCollection tc = new TestCollection(16);
+		int numtest = 16;
+		if(args.length > 0) {
+			numtest = Integer.parseInt(args[0]);
+			
+			numtest = numtest < 1 ? 1 : numtest;
+			numtest = numtest > 65536 ? 65536 : numtest;
+		}
+		
+		TestCollection tc = new TestCollection(numtest);
 		String test = tc.generateCode();
 	
 		System.out.println(test);
